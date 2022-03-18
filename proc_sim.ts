33cdd2d5000706a5b4f34028f7b0859cbd9fd1d7
Ben Thompson <benjfthompson@gmail.com>
2022-03-18 17:04:24 +0000

    Should have committed more. Everything works, just need to improve description. maybe add a text editor/compiler outside the ram entry

--- proc_sim.ts
+++ proc_sim.ts
@@ -1,27 +1,8 @@
-interface Updatable {
-    update()
-}
-
 abstract class ReadOnlyBus {
     readonly size: number
     readonly maxValue: number
     protected value: number = 0
     protected dirty: boolean = false
-
-    protected listeners: Updatable[] = []
-    public addBusListener(listener: Updatable) {
-        this.listeners.push(listener);
-    }
-
-    public removeBusListener(listener: Updatable): boolean {
-        let oldLen = this.listeners.length
-        this.listeners = this.listeners.filter(v => v !== listener)
-        return this.listeners.length !== oldLen
-    }
-
-    public getBusListeners(): Updatable[] {
-        return [...this.listeners] // return shallow copy to prevent external mutation
-    }
 
     constructor(size: number) {
         if (size > 31) {
@@ -73,97 +54,75 @@ class Bus extends ReadOnlyBus {
 const CTRL_SET = 0b001
 const CTRL_ENB = 0b010
 const CTRL_INC = 0b100 // only used by program counter
-class Register extends ReadOnlyBus implements Updatable {
-    private dataBus: Bus
-    private controlBus: Bus
+class Register extends ReadOnlyBus {
+    public readonly dataBus: Bus
+    public readonly controlBus: Bus
+    public readonly name: string
 
-    public constructor(size: number, inputBus: Bus, controlBus: Bus) {
+    public constructor(size: number, name: string, inputBus: Bus, controlBus: Bus) {
         super(size)
 
-        this.setInput(inputBus)
-        this.setControl(controlBus)
-    }
+        this.name = name
 
-    public setInput(inputBus: Bus) {
         this.dataBus = inputBus
-    }
-
-    public setControl(controlBus: Bus) {
         this.controlBus = controlBus
     }
 
-    public update() {
-        switch (this.controlBus.getValue() & 0b111) {
-            case CTRL_SET:
-                this.value = this.dataBus.getValue()
-                this.dirty = true
-                break
-            case CTRL_ENB:
-                this.dataBus.setValue(this.value)
-                break
-            case CTRL_INC:
-                this.value++
-                this.dirty = true
+    public update_enable() {
+        if (this.controlBus.getValue() & CTRL_ENB) {
+            this.dataBus.setValue(this.value)
+        }
+    }
+
+    public update_set() {
+        let ctrl = this.controlBus.getValue()
+        if (ctrl & CTRL_SET) {
+            this.value = this.dataBus.getValue()
+            this.dirty = true
+        } else if (ctrl & CTRL_INC && !this.isDirty()) {
+            this.value = (this.value + 1) % (1 << this.size)
+            this.dirty = true
         }
     }
 }
 
-// class Multiplexer extends Bus implements Updatable {
-//     private inputBusses: Bus[]
-//     private controlBus: Bus
-//
-//     public constructor(size: number, controlBus: Bus = null, ...inputBusses: Bus[]) {
-//         super(size)
-//         this.setControl(controlBus)
-//         this.setInputs(...inputBusses)
-//     }
-//
-//     public setControl(controlBus: Bus) {
-//         this.controlBus = controlBus
-//     }
-//
-//     public setInputs(...inputBusses: Bus[]) {
-//         if (inputBusses != null && inputBusses.length > 0) {
-//             if (this.controlBus == null){
-//                 console.log(`W:\tInputs defined, but no control to select them!`)
-//             } else if (this.controlBus.maxValue+1 < inputBusses.length){
-//                 console.log(`W:\tControl Bus too small for number of inputs!`)
-//             }
-//         }
-//         this.inputBusses = inputBusses
-//     }
-//
-//     public setValue(value: number) {
-//         console.log(`W:\tCannot set value of Multiplexer!`)
-//     }
-//
-//     public getValue(): number {
-//         return this.inputBusses[this.controlBus.getValue()].getValue()
-//     }
-//
-//     public getBits(): boolean[] {
-//         return this.inputBusses[this.controlBus.getValue()].getBits()
-//     }
-// }
-
-const ALU_ADD  = 0b0001
-const ALU_SUB  = 0b0010
-const ALU_AND  = 0b0011
-const ALU_OR   = 0b0100
-const ALU_XOR  = 0b0101
-const ALU_NOT  = 0b0110
+const ALU_ADD  = 0b001
+const ALU_SUB  = 0b010
+const ALU_AND  = 0b011
+const ALU_OR   = 0b100
+const ALU_XOR  = 0b101
+const ALU_NOT  = 0b110
+const ALU_INSTRUCTIONS = {
+    "add": ALU_ADD,
+    "sub": ALU_SUB,
+    "and": ALU_AND,
+    "ior": ALU_OR,
+    "xor": ALU_XOR,
+    "not": ALU_NOT
+}
 
 const FLGS_Z = 0b1000
 const FLGS_S = 0b0100
 const FLGS_O = 0b0010
 const FLGS_C = 0b0001
-class ArithmeticLogicUnit implements Updatable {
-    private inputA: Bus
-    private inputB: Bus
+class ArithmeticLogicUnit extends ReadOnlyBus {
+    private readonly inputT1: ReadOnlyBus
+    private readonly inputT2: ReadOnlyBus
 
-    private dataBus: Bus
-    private controlBus: Bus
-    private flags: Bus
+    private readonly dataBus: Bus
+    private readonly controlBus: ReadOnlyBus
+
+    public constructor(t1: ReadOnlyBus, t2: ReadOnlyBus, dataBus: Bus, controlBus: ReadOnlyBus) {
+        super(4)
+        this.inputT1 = t1
+        this.inputT2 = t2
+        this.dataBus = dataBus
+
+        this.controlBus = controlBus
+
+        // this.value = flags
+    }
+
 
     public update() {
         let op = this.controlBus.getValue()
@@ -171,11 +130,11 @@ class ArithmeticLogicUnit implements Updatable {
             return
         }
 
-        let a = this.inputA.getValue()
-        let signA: number = +((a & (1<<(this.inputA.size-1))) != 0)
+        let a = this.inputT1.getValue()
+        let signA: number = +((a & (1<<(this.inputT1.size-1))) != 0)
 
-        let b = this.inputB.getValue()
-        let signB: number = +((b & (1<<(this.inputB.size-1))) != 0)
+        let b = this.inputT2.getValue()
+        let signB: number = +((b & (1<<(this.inputT2.size-1))) != 0)
 
         let res: number = 0
         let flgs = 0b0000
@@ -206,40 +165,53 @@ class ArithmeticLogicUnit implements Updatable {
             res -= this.dataBus.maxValue
             flgs |= FLGS_C // 1 << 0 // set carry flag
         }
-        let signRes = +(1 & (res>>(this.dataBus.size-1)))
-        flgs |= FLGS_S // signRes << 2 // set sign flag
+        let signRes = (res & (1 << (this.dataBus.size - 1))) > 0 ? 1 : 0
+        flgs |= signRes ? FLGS_S : 0 // set sign flag
 
         if (signA === signB) {
-            flgs |= FLGS_O * (signA ^ signRes) // (signRes ^ signA) << 1 // set overflow flag
+            flgs |= (signA ^ signRes) ? FLGS_O : 0 // (signRes ^ signA) << 1 // set overflow flag
         }
 
         if (res === 0) {
             flgs |= FLGS_Z // 1 << 3 // set zero flag
         }
         this.dataBus.setValue(res)
-        this.flags.setValue(flgs)
+        this.value = flgs
+        this.dirty = true
     }
 }
 
-const RAM_READ = 0b01
-const RAM_WRITE = 0b10
-class RAM  implements Updatable {
-    private controlBus: Bus // [write, read]
-    private addressBus: Bus
+const RAM_READ = 0b001
+const RAM_WRITE = 0b010
+const RAM_INSTRUCTIONS = {
+    "load": RAM_READ,
+    "save": RAM_WRITE
+}
+
+class RAM {
+    private controlBus: ReadOnlyBus
+    private addressBus: ReadOnlyBus
 
     private dataBus: Bus
 
-    private data: number[]
+    public readonly data: number[]
+
+    public constructor(wordSize: number, controlBus: ReadOnlyBus, addressBus: ReadOnlyBus, dataBus: Bus) {
+        this.controlBus = controlBus
+        this.addressBus = addressBus
+        this.dataBus = dataBus
+        this.data = Array.from({length: (1<<wordSize)}, () => 0)
+    }
 
     public update() {
         let address = this.addressBus.getValue()
         let value: number
         switch (this.controlBus.getValue()) {
-            case RAM_READ:
+            case CTRL_ENB:
                 value = this.data[address]
                 this.dataBus.setValue(value)
                 break
-            case RAM_WRITE:
+            case CTRL_SET:
                 value = this.dataBus.getValue()
                 this.data[address] = value
                 break
@@ -262,6 +234,12 @@ const INS_JMP_JMP = 0b000
 const INS_JMP_JLZ = 0b001
 const INS_JMP_JGZ = 0b010
 const INS_JMP_JEZ = 0b011
+const JMP_INSTRUCTIONS = {
+    "jmp": INS_JMP_JMP,
+    "jlz": INS_JMP_JLZ,
+    "jgz": INS_JMP_JGZ,
+    "jez": INS_JMP_JEZ
+}
 
 type funcFunc = () => funcFunc
 class Decoder {
@@ -278,6 +256,8 @@ class Decoder {
     private flags: ReadOnlyBus
 
     private currentInstruction: funcFunc = this.fetch_step1
+    public readonly currentStep = () =>this.currentInstruction.name
+
 
     private fetch_step1(): funcFunc {
         this.programControl.setValue(CTRL_ENB)
@@ -421,6 +401,12 @@ class Decoder {
         this.registerT1Control.setValue(0)
         this.registerT2Control.setValue(0)
 
+        // "not" only needs one operand
+        let ins = this.instructionBus.getValue()
+        let ins_body = (ins >> 3) & 0b111
+        if (ins_body == ALU_NOT) {
+            return this.execute_alu_step7
+        }
         return this.execute_alui_step5
     }
 
@@ -458,9 +444,9 @@ class Decoder {
         let ins_t1 = 1 & (ins >> 2)
 
         if (ins_t1) {
-            this.registerAControl.setValue(CTRL_ENB)
-        } else {
             this.registerBControl.setValue(CTRL_ENB)
+        } else {
+            this.registerAControl.setValue(CTRL_ENB)
         }
         this.registerT1Control.setValue(CTRL_SET)
 
@@ -472,6 +458,12 @@ class Decoder {
         this.registerBControl.setValue(0)
         this.registerT1Control.setValue(0)
 
+        // "not" only needs one operand
+        let ins = this.instructionBus.getValue()
+        let ins_body = (ins >> 3) & 0b111
+        if (ins_body == ALU_NOT) {
+            return this.execute_alu_step7
+        }
         return this.execute_alur_step3
     }
 
@@ -480,9 +472,9 @@ class Decoder {
         let ins_t2 = 1 & (ins >> 1)
 
         if (ins_t2) {
-            this.registerAControl.setValue(CTRL_ENB)
-        } else {
             this.registerBControl.setValue(CTRL_ENB)
+        } else {
+            this.registerAControl.setValue(CTRL_ENB)
         }
         this.registerT2Control.setValue(CTRL_SET)
 
@@ -593,4 +585,170 @@ class Decoder {
     public update() {
         this.currentInstruction = this.currentInstruction()
     }
+
+    public constructor(instructionControl: Bus,
+                       instructionBus: ReadOnlyBus,
+                       programControl: Bus,
+                       ramControl: Bus,
+                       registerAControl: Bus,
+                       registerBControl: Bus,
+                       registerT1Control: Bus,
+                       registerT2Control: Bus,
+                       addressControl: Bus,
+                       aluControl: Bus,
+                       flags: ReadOnlyBus) {
+        this.instructionControl = instructionControl
+        this.instructionBus = instructionBus
+        this.programControl = programControl
+        this.ramControl = ramControl
+        this.registerAControl = registerAControl
+        this.registerBControl = registerBControl
+        this.registerT1Control = registerT1Control
+        this.registerT2Control = registerT2Control
+        this.addressControl = addressControl
+        this.aluControl = aluControl
+        this.flags = flags
+    }
+}
+
+function assemble_alu_ins(ins: string): number[] | null {
+    let pattern = `(${Object.keys(ALU_INSTRUCTIONS).filter(v => v != 'not').join('|')}) (?:(a|b) (a|b)|(-?\\d{1,3}) (a|b)|(a|b) (-?\\d{1,3})) (a|b)|not (-?\\d{1,3}|a|b) (a|b)`
+    let match = ins.toLowerCase().match(pattern)
+    if (match == null) {
+        return null
+    }
+
+    match = match.slice(1).filter(v => v != null)
+
+    let ins_name = match[0]
+
+    let instruction: number[] = [0]
+
+    let prefix = 0
+    let infix = ALU_INSTRUCTIONS[ins_name]
+    let t1 = 0
+    let t2 = 0
+    let d = 0
+    let immediate_value: number | null = null
+
+    if (ins_name == 'not') {
+        let op = match[1]
+        if (op.match("-?\\d+")) {
+            prefix = INS_ALU_I
+            immediate_value = parseInt(op, 10)
+        } else {
+            prefix = INS_ALU_R
+        }
+        let dest = match[2]
+        d = dest == 'a' ? 0 : 1
+    } else {
+        let op1 = match[1]
+        let op2 = match[2]
+        if (op1.match("-?\\d+") || op2.match("-?\\d+")) {
+            prefix = INS_ALU_I
+            if (op1.match("-?\\d+")) {
+                immediate_value = parseInt(op1, 10)
+                t1 = 0
+            } else {
+                immediate_value = parseInt(op2, 10)
+                t1 = 1
+            }
+        } else {
+            prefix = INS_ALU_R
+        }
+        if (op1 == 'b' && immediate_value == null) {
+            t1 = 1
+        } else if (op2 == 'b' && immediate_value == null) {
+            t2 = 1
+        } else if (immediate_value != null) {
+            t2 = (op1 == 'a' || op2 == 'a') ? 0 : 1
+        }
+        let dest = match[3]
+        d = dest == 'a' ? 0 : 1
+    }
+
+    if (immediate_value != null) {
+        instruction[1] = immediate_value & ((1 << WORD_SIZE) - 1)
+    }
+
+    instruction[0] = (prefix << 6) | (infix << 3) | (t1 << 2) | (t2 << 1) | d
+    return instruction
+}
+
+function assemble_jmp_ins(ins: string): number[] | null {
+    let pattern = `(${Object.keys(JMP_INSTRUCTIONS).join('|')}) ([\\da-f]{2}|a|b)`
+    let match = ins.toLowerCase().match(pattern)
+    if (match == null) {
+        return null
+    }
+
+    match = match.slice(1).filter(v => v != null)
+
+    let ins_name = match[0]
+
+    let instruction: number[] = [1]
+
+    let prefix = INS_JMP
+    let infix = JMP_INSTRUCTIONS[ins_name]
+    let t1 = 0
+    let t2 = 0
+    let d = 0
+
+    let op = match[1]
+    if (op.match("[\\da-f]{2}")) {
+        instruction[1] = parseInt(op, 16)
+        t1 = 1
+    } else if (op == 'b') {
+        t2 = 1
+    }
+
+    instruction[0] = (prefix << 6) | (infix << 3) | (t1 << 2) | (t2 << 1) | d
+    return instruction
+}
+
+function assemble_ram_ins(ins: string): number[] | null {
+    let pattern = `(${Object.keys(RAM_INSTRUCTIONS).join('|')}) (-?[\\da-f]{1,3}|a|b) (a|b)`
+    let match = ins.toLowerCase().match(pattern)
+    if (match == null) {
+        return null
+    }
+
+    match = match.slice(1).filter(v => v != null)
+
+    let ins_name = match[0]
+
+    let instruction: number[] = [0]
+
+    let prefix = INS_RAM
+    let infix = RAM_INSTRUCTIONS[ins_name]
+    let t1 = 0
+    let t2 = match[1] == 'b' ? 1:0
+    let d = match[2] == 'b' ? 1 : 0
+
+    let op = match[1]
+    if (op.match("-?\\d{1,3}") && ins_name == "load") {
+        instruction[1] = parseInt(op, 10) & ((1 << WORD_SIZE) - 1)
+        t1 = 1
+    } else if (op.match("[\\da-f]{1,2}") && ins_name == "save") {
+        instruction[1] = parseInt(op, 16) & ((1 << WORD_SIZE) - 1)
+    } else if (op != 'a' && op != 'b') {
+        return null
+    }
+
+    instruction[0] = (prefix << 6) | (infix << 3) | (t1 << 2) | (t2 << 1) | d
+    return instruction
+}
+
+function assemble_ins(ins: string): number[] | null {
+    let instruction = assemble_alu_ins(ins)
+    if (instruction != null) {
+        return instruction
+    }
+
+    instruction = assemble_jmp_ins(ins)
+    if (instruction != null) {
+        return instruction
+    }
+
+    return assemble_ram_ins(ins)
 }
---
+++ sim_ui.ts
@@ -0,0 +1,268 @@
+
+const WORD_SIZE = 8
+
+const CTRL_SIZE_SET = 1
+const CTRL_SIZE_SET_ENB = 2
+const CTRL_SIZE_SET_ENB_INC = 3
+
+const ALU_CTRL_SIZE = 3
+
+type SVGinHTML = HTMLElement & SVGElement
+type clockState = "loop" | "instruction" | "step"
+
+class Simulation {
+    private readonly dataBus: Bus
+
+    private readonly ctrlIns: Bus
+    private readonly regIns: Register
+
+    private readonly ctrlPC: Bus
+    private readonly regPC: Register
+
+    private readonly ctrlRam: Bus
+    private readonly ram: RAM
+
+    private readonly ctrlAddress: Bus
+    private readonly regAddress: Register
+
+    private readonly ctrlA: Bus
+    private readonly regA: Register
+
+    private readonly ctrlB: Bus
+    private readonly regB: Register
+
+    private readonly ctrlT1: Bus
+    private readonly regT1: Register
+
+    private readonly ctrlT2: Bus
+    private readonly regT2: Register
+
+    private readonly ctrlALU: Bus
+    private readonly alu: ArithmeticLogicUnit
+
+    private readonly decoder: Decoder
+
+    private readonly registers: Register[]
+    private readonly busses: ReadOnlyBus[]
+
+    public constructor() {
+        this.dataBus = new Bus(WORD_SIZE)
+
+        this.ctrlIns = new Bus(CTRL_SIZE_SET)
+        this.regIns = new Register(WORD_SIZE, "ins", this.dataBus, this.ctrlIns)
+
+        this.ctrlPC = new Bus(CTRL_SIZE_SET_ENB_INC)
+        this.regPC = new Register(WORD_SIZE, "pc", this.dataBus, this.ctrlPC)
+
+        this.ctrlAddress = new Bus(CTRL_SIZE_SET)
+        this.regAddress = new Register(WORD_SIZE, "addr", this.dataBus, this.ctrlAddress)
+
+        this.ctrlRam = new Bus(CTRL_SIZE_SET_ENB)
+        this.ram = new RAM(WORD_SIZE, this.ctrlRam, this.regAddress, this.dataBus)
+        this.initializeRamDisplay()
+
+        this.ctrlA = new Bus(CTRL_SIZE_SET_ENB)
+        this.regA = new Register(WORD_SIZE, "a", this.dataBus, this.ctrlA)
+
+        this.ctrlB = new Bus(CTRL_SIZE_SET_ENB)
+        this.regB = new Register(WORD_SIZE, "b", this.dataBus, this.ctrlB)
+
+        this.ctrlT1 = new Bus(CTRL_SIZE_SET)
+        this.regT1 = new Register(WORD_SIZE, "t1", this.dataBus, this.ctrlT1)
+
+        this.ctrlT2 = new Bus(CTRL_SIZE_SET)
+        this.regT2 = new Register(WORD_SIZE, "t2", this.dataBus, this.ctrlT2)
+
+        this.ctrlALU = new Bus(ALU_CTRL_SIZE)
+        this.alu = new ArithmeticLogicUnit(this.regT1, this.regT2, this.dataBus, this.ctrlALU)
+
+        this.decoder = new Decoder(this.ctrlIns, this.regIns, this.ctrlPC, this.ctrlRam, this.ctrlA, this.ctrlB,
+            this.ctrlT1, this.ctrlT2, this.ctrlAddress, this.ctrlALU, this.alu)
+
+        this.registers = [this.regIns, this.regPC, this.regA, this.regB, this.regT1, this.regT2, this.regAddress]
+        this.busses = [...this.registers,
+            this.ctrlIns, this.ctrlPC, this.ctrlA, this.ctrlB, this.ctrlT1, this.ctrlT2, this.ctrlAddress,
+            this.ctrlALU, this.alu, this.ctrlRam, this.dataBus
+        ]
+    }
+
+    private initializeRamDisplay() {
+        let ramTBL = document.getElementById("ram-table")
+        let ramHTML = (i: number, v: number) => `
+<tr class="ram-word">
+  <td class="inputLabel">${i.toString(16).padStart(2, '0')}</td>
+  <td><input id="ram-input${i}" class="inputArea" size="8" pattern="[01]{${WORD_SIZE}}" inputmode="numeric" value="${v.toString(2).padStart(WORD_SIZE, '0')}"></td>
+</tr>`
+        let innerHTML = ''
+        this.ram.data.forEach((value, index) => {
+            let rowHTML = ramHTML(index, value)
+            innerHTML += rowHTML
+        })
+        ramTBL.innerHTML += innerHTML
+
+        let inputs = Array.from(document.getElementsByClassName("inputArea"))
+        inputs.forEach((elem, index) => {
+            elem.addEventListener("change", (e) => {
+                let target = e.target as HTMLInputElement
+                let idx = parseInt(target.id.replace("ram-input", ""))
+                if (!target.value.match("[01]{8}")) {
+                    let ins = assemble_ins(target.value)
+                    if (ins == null || (ins.length > 1 && idx > (1<<WORD_SIZE)-1-ins.length)) {
+                        target.value = this.ram.data[idx].toString(2).padStart(WORD_SIZE, '0')
+                    } else {
+                        while (ins.length > 0) {
+                            let textBox = document.getElementById(`ram-input${idx}`) as HTMLInputElement
+                            let value = ins.shift()
+                            textBox.value = value.toString(2).padStart(WORD_SIZE, '0')
+                            this.ram.data[idx] = value
+                            idx++
+                        }
+                    }
+                } else {
+                    let value = parseInt(target.value, 2)
+                    if (value > (1<<WORD_SIZE)) {
+                        value &= (1<<WORD_SIZE) - 1
+                        target.value = value.toString(2).padStart(WORD_SIZE, '0')
+                    }
+                    // alert(`row ${idx} edited!`)
+                    this.ram.data[idx] = value
+                }
+            })
+        })
+    }
+
+    private cleanBusses = () => this.busses.forEach(b => b.clean())
+    public update_state() {
+        this.cleanBusses()
+        this.decoder.update()
+
+        this.dataBus.setValue(0)
+        this.dataBus.clean()
+
+        this.registers.forEach(r => r.update_enable())
+        this.registers.forEach(r => r.update_set())
+
+        this.alu.update()
+        this.ram.update()
+
+        this.registers.forEach(r => r.update_set())
+    }
+
+    private setRamScroll() {
+        let ramContainer = document.getElementById("ram-table").parentElement
+        let ramIndex = this.regAddress.getValue()
+        let ramRow = document.getElementById(`ram-input${ramIndex}`).parentElement.parentElement
+        ramRow.classList.add("active")
+
+        ramContainer.scrollTo(0, ramRow.offsetTop - ramRow.offsetHeight/2 - ramContainer.offsetHeight/2)
+    }
+
+    public update_display() {
+        function update_bus(name: string, suffixes: string[], value: number) {
+            for (let i = 0; i <= suffixes.length; i++) {
+                let line = document.getElementById(`${name}${suffixes[i]}`)
+                if (line == null) {
+                    continue
+                }
+
+                line.classList.remove('high')
+                if (value & (1 << i)) {
+                    line.classList.add('high')
+                }
+            }
+        }
+
+        let data_value = this.dataBus.getValue()
+        update_bus("data-line", "01234567".split(""), data_value)
+
+        this.registers.forEach(r => {
+            update_bus(`${r.name}-line`, "01234567".split(""), r.getValue())
+            update_bus(`${r.name}-ctrl`, "-s -e -i".split(" "), r.controlBus.getValue())
+            let textValue = document.getElementById(`${r.name}-value`) as SVGinHTML;
+            textValue.textContent = r.getValue().toString(2).padStart(r.size, '0')
+        })
+
+        update_bus("alu-ctrl", "-0 -1 -2".split(" "), this.ctrlALU.getValue())
+        update_bus("flgs", "-0 -1 -2 -3".split(" "), this.alu.getValue())
+        update_bus("ram-ctrl", "-s -e".split(" "), this.ctrlRam.getValue())
+
+        this.ram.data.forEach((value, index) => {
+            let inputBox = document.getElementById(`ram-input${index}`) as HTMLInputElement
+            inputBox.parentElement.parentElement.classList.remove("active")
+            if (inputBox !== document.activeElement) {
+                inputBox.value = value.toString(2).padStart(WORD_SIZE, '0')
+            }
+        })
+        this.setRamScroll()
+    }
+
+    public update() {
+        if (this._state == null) {
+            this._state = "step"
+        }
+        this.update_state()
+        this.update_display()
+        if (this._state == "step") {
+            this._state = null
+        }
+    }
+
+    public async update_loop() {
+        if (this._state == null) {
+            this._state = "instruction"
+            this._stop = false
+        }
+        this.update()
+        while (this.decoder.currentStep() != "fetch_step1" && !this._stop) {
+            await sleep(500)
+            this.update()
+        }
+        if (this._state == "instruction") {
+            this._state = null
+        }
+    }
+
+
+    private _stop: boolean = false
+    public readonly stop = () => this._stop = true
+    private _state: clockState | null = null
+    public readonly state = () => this._state
+    public async loop() {
+        this._state = "loop"
+        this._stop = false
+        while (!this._stop) {
+            await this.update_loop()
+        }
+        this._state = null
+    }
+}
+
+const sleep = ms => new Promise(r => setTimeout(r, ms))
+
+let sim = new Simulation()
+
+async function onButtonPress(button: clockState) {
+    let currentState = sim.state()
+    sim.stop()
+    while (sim.state() != null) {
+        await sleep(1)
+    }
+
+    if (currentState == button) {
+        return
+    }
+    let buttonGroup = document.getElementById(`clock-${button}`)
+
+    buttonGroup.classList.add("active")
+    switch (button) {
+        case "step":
+            sim.update()
+            break
+        case "instruction":
+            await sim.update_loop()
+            break
+        case "loop":
+            await sim.loop()
+    }
+    buttonGroup.classList.remove("active")
+}
